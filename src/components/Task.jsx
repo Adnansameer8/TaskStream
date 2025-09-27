45e907134d2dba9f750b362ba1efb28a11bb872f
Adnan Sameer <adnansameeradnan8@gmail.com>
2025-09-28 00:02:29 +0530

    Changed title

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,6 +5,7 @@ export default function Task({task, sourceCol}) {
         e.dataTransfer.setData("task", task);
         e.dataTransfer.setData("sourceCol",sourceCol);
     };
+    
 
     return(
         <div className="task" draggable onDragStart={dragStart}>
